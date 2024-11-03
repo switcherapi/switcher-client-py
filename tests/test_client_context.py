98769348d1fbd11bfaed60d01e44559bac1b57ab
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-11-03 15:32:31 -0800

    Added Client.build_context to handle initial context params (#1)

--- tests/test_client_context.py
+++ tests/test_client_context.py
@@ -1,2 +1,29 @@
+import pytest
+
+from switcher_client import Client
+
 def test_context():
-    assert True
+    Client.build_context(
+        domain='My Domain',
+        url='https://api.switcherapi.com',
+        api_key='[API_KEY]',
+        component='MyApp'
+    )
+
+    try:
+        Client.verify_context()
+    except ValueError as e:
+        pytest.fail(f'Context verification failed: {e}')
+
+def test_clear_context():
+    Client.build_context(
+        domain='My Domain',
+        url='https://api.switcherapi.com',
+        api_key='[API_KEY]',
+        component='MyApp'
+    )
+
+    Client.clear_context()
+
+    with pytest.raises(ValueError):
+        Client.verify_context()
