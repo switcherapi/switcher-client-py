cc34a8c03ad2a7555f51b127fb8594b7e2ba1a9b
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-12-25 15:34:01 -0800

    Added ContextOptions for optional settings (#2)
    * Added ContextOptions for optional settings
    
    * chore: updated SonarQube action

--- switcher_client/client.py
+++ switcher_client/client.py
@@ -1,16 +1,19 @@
-from switcher_client.context import Context
+from typing import Optional
+
+from switcher_client.context import Context, ContextOptions
 from switcher_client.context import DEFAULT_ENVIRONMENT
 
 class Client:
-    context = None
+    context: Optional[Context] = None
 
     @staticmethod
     def build_context(
         domain: str, 
-        url: str,
-        api_key: str,
-        component: str,
-        environment: str | None = DEFAULT_ENVIRONMENT):
+        url: Optional[str] = None,
+        api_key: Optional[str] = None,
+        component: Optional[str] = None,
+        environment: Optional[str] = DEFAULT_ENVIRONMENT,
+        options = ContextOptions()):
         """ 
         Build the context for the client 
 
@@ -19,9 +22,10 @@ class Client:
         :param api_key: Switcher-API key generated for the application/component
         :param component: Application/component name
         :param environment: Environment name
+        :param options: Optional parameters
             
         """
-        Client.context = Context(domain, url, api_key, component, environment)
+        Client.context = Context(domain, url, api_key, component, environment, options)
 
     @staticmethod
     def clear_context():
--- switcher_client/context.py
+++ switcher_client/context.py
@@ -1,9 +1,16 @@
 DEFAULT_ENVIRONMENT = 'default'
+DEFAULT_LOCAL = False
+
+class ContextOptions:
+    def __init__(self, local = DEFAULT_LOCAL, snapshot_location = None):
+        self.local = local
+        self.snapshot_location = snapshot_location
 
 class Context:
-    def __init__(self, domain, url, api_key, component, environment):
+    def __init__(self, domain, url, api_key, component, environment, options = ContextOptions()):
         self.domain = domain
         self.url = url
         self.api_key = api_key
         self.component = component
         self.environment = environment
+        self.options = options
