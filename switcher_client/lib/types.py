94013a4fbe858783ef0f04390ceff8396530780c
petruki <31597636+petruki@users.noreply.github.com>
2025-08-03 20:22:09 -0700

    wip: added snapshot loading from file feature

--- switcher_client/lib/types.py
+++ switcher_client/lib/types.py
@@ -1,5 +1,23 @@
+from typing import Optional, List
+
 class ResultDetail:
     def __init__(self, result: bool, reason: str, metadata: dict):
         self.result = result
         self.reason = reason
         self.metadata = metadata
+
+class Snapshot:
+    def __init__(self):
+        self.data: SnapshotData
+
+class SnapshotData:
+    def __init__(self):
+        self.domain: Domain
+
+class Domain:
+    def __init__(self):
+        self.name: Optional[str] = None
+        self.version: int = 0
+        self.activated: Optional[bool] = None
+
+
---
+++ switcher_client/lib/utils.py
@@ -0,0 +1,3 @@
+def get(value, default_value):
+    """ Return value if not None, otherwise return default_value """
+    return value if value is not None else default_value
