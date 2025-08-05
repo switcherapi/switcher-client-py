1be1491a4c0c5c39223653c3094d9f0babdc2cec
petruki <31597636+petruki@users.noreply.github.com>
2025-08-04 17:17:56 -0700

    Added load_domain API to load from snapshot file

--- tests/test_client_context.py
+++ tests/test_client_context.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from switcher_client import Client, ContextOptions
@@ -44,14 +45,14 @@ def test_context_with_optionals():
         domain='My Domain',
         options=ContextOptions(
             local=True,
-            snapshot_location='./snapshots'
+            snapshot_location='./tests/snapshots'
         )
     )
 
     options = Client.context.options
 
     assert options.local == True
-    assert options.snapshot_location == './snapshots'
+    assert options.snapshot_location == './tests/snapshots'
 
 def test_load_from_snapshot():
     """ Should load Domain from snapshot file """
@@ -60,7 +61,7 @@ def test_load_from_snapshot():
         domain='My Domain',
         options=ContextOptions(
             local=True,
-            snapshot_location='./snapshots'
+            snapshot_location='./tests/snapshots'
         )
     )
 
@@ -71,3 +72,33 @@ def test_load_from_snapshot():
     version = Client.load_snapshot()
     assert Client.snapshot_version() == 1
     assert version == Client.snapshot_version()
+
+def test_load_from_snapshot_empty():
+    """ Should create clean snapshot when no snapshot file exists """
+
+    Client.build_context(
+        domain='My Domain',
+        environment='generated-clean',
+        options=ContextOptions(
+            local=True,
+            snapshot_location='./tests/snapshots'
+        )
+    )
+
+    # verify initial snapshot version
+    assert Client.snapshot_version() == 0
+
+    # test
+    version = Client.load_snapshot()
+    assert Client.snapshot_version() == 0
+    assert version == Client.snapshot_version()
+
+    # tear down
+    delete_snapshot_file('./tests/snapshots', 'generated-clean')
+    
+# Helpers
+
+def delete_snapshot_file(snapshot_location: str, environment: str):
+    snapshot_file = f"{snapshot_location}/{environment}.json"
+    if os.path.exists(snapshot_file):
+        os.remove(snapshot_file)
