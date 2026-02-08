583aa66e64a7e4414980accbdb6578690074a933
petruki <31597636+petruki@users.noreply.github.com>
2026-02-08 11:07:36 -0800

    feat: added Client.subscribe_notify_error() for error monitoring

--- tests/test_switcher_silent_mode.py
+++ tests/test_switcher_silent_mode.py
@@ -6,6 +6,8 @@ from pytest_httpx import HTTPXMock
 from switcher_client import Client
 from switcher_client.lib.globals.global_context import ContextOptions
 
+async_error = None
+
 def test_silent_mode_for_check_criteria(httpx_mock):
     """ Should use the silent mode when the remote API is not available for check criteria """
 
@@ -15,15 +17,19 @@ def test_silent_mode_for_check_criteria(httpx_mock):
     given_check_health(httpx_mock, status=500)
     given_context(silent_mode='1s')
 
+    Client.subscribe_notify_error(lambda error: globals().update(async_error=str(error)))
     switcher = Client.get_switcher('FF2FOR2022')
 
     # test
     # assert silent mode being used while registering the error
     assert switcher.is_on('FF2FOR2022')
+    assert async_error == '[check_criteria] failed with status: 429'
 
     # assert silent mode being used in the next call
     time.sleep(1.5)
+    globals().update(async_error=None)
     assert switcher.is_on('FF2FOR2022')
+    assert async_error is None
 
 def test_silent_mode_for_check_criteria_restabilished(httpx_mock):
     """ Should retry check criteria once the remote API is restabilished and the token is renewed """
@@ -33,17 +39,22 @@ def test_silent_mode_for_check_criteria_restabilished(httpx_mock):
     given_check_criteria(httpx_mock, key='FF2FOR2022', response={'error': 'Too many requests'}, status=429)
     given_context(silent_mode='1s')
 
+    Client.subscribe_notify_error(lambda error: globals().update(async_error=str(error)))
     switcher = Client.get_switcher('FF2FOR2022')
 
     # test
     # assert silent mode being used while registering the error
     assert switcher.is_on('FF2FOR2022')
+    assert async_error == '[check_criteria] failed with status: 429'
 
     # assert from remote API once the API is restabilished and the token is renewed
     given_check_criteria(httpx_mock, key='FF2FOR2022', response={'result': True}, status=200)
     given_check_health(httpx_mock, status=200)
+
     time.sleep(1.5)
+    globals().update(async_error=None)
     assert switcher.is_on('FF2FOR2022')
+    assert async_error is None
 
 # Helpers
 
@@ -85,4 +96,3 @@ def given_check_health(httpx_mock: HTTPXMock, status=200):
         method='GET',
         status_code=status,
     )
-
