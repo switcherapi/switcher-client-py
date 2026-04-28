4179692f1152b4bad49c5e0ff6fa7aa6a4c5ad99
Roger Floriano <31597636+petruki@users.noreply.github.com>
2026-04-27 19:15:37 -0700

    feat: added support for conditional strategy mocking (#51)

--- switcher_client/__init__.py
+++ switcher_client/__init__.py
@@ -2,10 +2,12 @@ from switcher_client.client import Client
 from switcher_client.switcher import Switcher
 from switcher_client.lib.globals.global_context import ContextOptions
 from switcher_client.lib.snapshot_watcher import WatchSnapshotCallback
+from switcher_client.lib.snapshot import StrategiesType
 
 __all__ = [
     'Client',
     'Switcher',
     'ContextOptions',
     'WatchSnapshotCallback',
+    'StrategiesType'
 ]
