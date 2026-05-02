df74101fd4b574393e9ce794ce998d0c3283deb4
Roger Floriano <31597636+petruki@users.noreply.github.com>
2026-05-01 17:27:23 -0700

    feat: added LoadSnapshotOptions, RetryOptions to package main (#53)

--- switcher_client/__init__.py
+++ switcher_client/__init__.py
@@ -1,6 +1,8 @@
 from switcher_client.client import Client
 from switcher_client.switcher import Switcher
 from switcher_client.lib.globals.global_context import ContextOptions
+from switcher_client.lib.globals.global_snapshot import LoadSnapshotOptions
+from switcher_client.lib.globals.global_retry import RetryOptions
 from switcher_client.lib.snapshot_watcher import WatchSnapshotCallback
 from switcher_client.lib.snapshot import StrategiesType
 
@@ -8,6 +10,8 @@ __all__ = [
     'Client',
     'Switcher',
     'ContextOptions',
+    'LoadSnapshotOptions',
+    'RetryOptions',
     'WatchSnapshotCallback',
-    'StrategiesType'
+    'StrategiesType',
 ]
